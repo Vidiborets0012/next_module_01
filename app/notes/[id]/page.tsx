87cecc443ca88705e03e3fd3d78e67396dff7728
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-11-30 22:27:23 +0200

    return the logic for reading the ID from the parameters, add (prefetch), use HydrationBoundary, output data in the client component (NoteDetails.client.tsx)

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,4 +1,10 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import { getSingleNote } from "@/lib/api";
+import NoteDetailsClient from "./NoteDetails.client";
 
 type Props = {
   params: Promise<{ id: string }>;
@@ -7,21 +13,34 @@ type Props = {
 const NoteDetails = async ({ params }: Props) => {
   const { id } = await params;
   // console.log("note id:", id);
-  const note = await getSingleNote(id);
+  // const note = await getSingleNote(id);
   // console.log(note);
 
-  const formattedDate = note.updatedAt
-    ? `Updated at: ${note.updatedAt}`
-    : `Created at: ${note.createdAt}`;
+  // const formattedDate = note.updatedAt
+  //   ? `Updated at: ${note.updatedAt}`
+  //   : `Created at: ${note.createdAt}`;
 
-  // return <div>NoteDetails</div>;
+  // // return <div>NoteDetails</div>;
+  // return (
+  //   <div>
+  //     <h2>{note.title}</h2>
+  //     <p>{note.content}</p>
+  //     <button>Edit</button>
+  //     <p>{formattedDate}</p>
+  //   </div>
+  // );
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => getSingleNote(id),
+  });
+
+  // return <NoteDetailsClient />;
   return (
-    <div>
-      <h2>{note.title}</h2>
-      <p>{note.content}</p>
-      <button>Edit</button>
-      <p>{formattedDate}</p>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient />
+    </HydrationBoundary>
   );
 };
 
