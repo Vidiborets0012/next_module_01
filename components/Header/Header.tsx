6a5678d20a40b96984ac6c5cffe5a72f8c65ce13
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-11-30 11:35:36 +0200

    add lecture notes (lesson 11)

---
+++ components/Header/Header.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import css from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={css.header}>
+      {/* <h2>NoteHub</h2> */}
+      <Link href="/" aria-label="Home">
+        NoteHub
+      </Link>
+      <nav aria-label="Main Navigation">
+        <ul className={css.navigation}>
+          {/* <li>Home</li> */}
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/notes">Notes</Link>
+          </li>
+          <li>
+            <Link href="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
