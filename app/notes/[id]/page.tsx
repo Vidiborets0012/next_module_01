ada5a6f0f31e9efd2af6fad433fed96e0a998900
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-11-30 21:39:32 +0200

    create a page for viewing the details of a single note, note query

---
+++ app/notes/[id]/page.tsx
@@ -0,0 +1,16 @@
+import { getSingleNote } from "@/lib/api";
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+const NoteDetails = async ({ params }: Props) => {
+  const { id } = await params;
+  console.log("note id:", id);
+  const note = await getSingleNote(id);
+  console.log(note);
+
+  return <div>NoteDetails</div>;
+};
+
+export default NoteDetails;
