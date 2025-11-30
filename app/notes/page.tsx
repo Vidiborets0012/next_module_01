f82f96d8d0794f97bd268e62a5c1d0c02a2f20ee
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-11-30 20:35:41 +0200

    return the logic for getting the list of notes to the server component (Notes)

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,30 +1,48 @@
-"use client";
+// клієнтський компонент:
+// "use client";
 
+// import NoteList from "@/components/NoteList/NoteList";
+// import { getNotes, Note } from "@/lib/api";
+// import { useState } from "react";
+
+// const Notes = () => {
+//   // const notes = await getNotes();
+//   // console.log("notes:", notes);
+
+//   const [notes, setNotes] = useState<Note[]>([]);
+
+//   const handleClick = async () => {
+//     const response = await getNotes();
+
+//     if (response?.notes) {
+//       setNotes(response.notes);
+//     }
+//   };
+
+//   // return <div>Notes</div>;
+//   return (
+//     <section>
+//       <h1>Notes List</h1>
+//       {/* {response?.notes?.length > 0 && <NoteList notes={response.notes} />} */}
+//       <button onClick={handleClick}>Get my notes</button>
+//       {notes.length > 0 && <NoteList notes={notes} />}
+//     </section>
+//   );
+// };
+
+// export default Notes;
+
+// серверний компонент:
 import NoteList from "@/components/NoteList/NoteList";
-import { getNotes, Note } from "@/lib/api";
-import { useState } from "react";
+import { getNotes } from "@/lib/api";
 
-const Notes = () => {
-  // const notes = await getNotes();
-  // console.log("notes:", notes);
+const Notes = async () => {
+  const response = await getNotes();
 
-  const [notes, setNotes] = useState<Note[]>([]);
-
-  const handleClick = async () => {
-    const response = await getNotes();
-
-    if (response?.notes) {
-      setNotes(response.notes);
-    }
-  };
-
-  // return <div>Notes</div>;
   return (
     <section>
       <h1>Notes List</h1>
-      {/* {response?.notes?.length > 0 && <NoteList notes={response.notes} />} */}
-      <button onClick={handleClick}>Get my notes</button>
-      {notes.length > 0 && <NoteList notes={notes} />}
+      {response?.notes?.length > 0 && <NoteList notes={response.notes} />}
     </section>
   );
 };
