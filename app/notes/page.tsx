58add48aebfda49ab163424c834a17eab1402cd7
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-11-30 20:20:27 +0200

    make the Notes page component client-side (‘use client’)

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,10 +1,32 @@
-import { getNotes } from "@/lib/api";
+"use client";
 
-const Notes = async () => {
-  const notes = await getNotes();
-  console.log("notes:", notes);
+import NoteList from "@/components/NoteList/NoteList";
+import { getNotes, Note } from "@/lib/api";
+import { useState } from "react";
 
-  return <div>Notes</div>;
+const Notes = () => {
+  // const notes = await getNotes();
+  // console.log("notes:", notes);
+
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  const handleClick = async () => {
+    const response = await getNotes();
+
+    if (response?.notes) {
+      setNotes(response.notes);
+    }
+  };
+
+  // return <div>Notes</div>;
+  return (
+    <section>
+      <h1>Notes List</h1>
+      {/* {response?.notes?.length > 0 && <NoteList notes={response.notes} />} */}
+      <button onClick={handleClick}>Get my notes</button>
+      {notes.length > 0 && <NoteList notes={notes} />}
+    </section>
+  );
 };
 
 export default Notes;
