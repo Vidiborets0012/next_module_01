defd576442db7bb2a01c7496411be0569325fb8d
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-12-07 10:27:46 +0200

    use not-found.tsx as a client component with redirection

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,38 @@
-import Link from "next/link";
+// import Link from "next/link";
+
+// const NotFound = () => {
+//   return (
+//     <div>
+//       <h1>404 - Page Not Found</h1>
+//       <p>Sorry, the page you&#39;re looking for doesn&#39;t exist.</p>
+//       <Link href="/">Go back home</Link>
+//     </div>
+//   );
+// };
+
+// export default NotFound;
+
+//***************************
+// клієнтський компонент із перенаправленням
+
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Редірект через 3 секунди
+    const timer = setTimeout(() => router.push("/"), 3000);
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
     <div>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you&#39;re looking for doesn&#39;t exist.</p>
-      <Link href="/">Go back home</Link>
+      <h1>404 - Сторінку не знайдено</h1>
+      <p>Вас буде перенаправлено на головну через кілька секунд…</p>
     </div>
   );
 };
