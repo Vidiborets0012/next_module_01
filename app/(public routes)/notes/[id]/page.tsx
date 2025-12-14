9353ac34670444096fefb602635051a827dc2d82
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-12-14 14:00:27 +0200

    add Open Graph and Twitter meta tags

--- app/(public routes)/notes/[id]/page.tsx
+++ app/(public routes)/notes/[id]/page.tsx
@@ -17,6 +17,27 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   return {
     title: `Note: ${note.title}`,
     description: note.content.slice(0, 30),
+    openGraph: {
+      title: `Note: ${note.title}`,
+      description: note.content.slice(0, 100),
+      url: `https://notehub.com/notes/${id}`,
+      siteName: "NoteHub",
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/og-meta.jpg",
+          width: 1200,
+          height: 630,
+          alt: note.title,
+        },
+      ],
+      type: "article",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${note.title}`,
+      description: note.content.slice(0, 3),
+      images: ["https://ac.goit.global/fullstack/react/og-meta.jpg"],
+    },
   };
 }
 
