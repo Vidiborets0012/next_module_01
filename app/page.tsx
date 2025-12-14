9ea5ca31175a4e2d57ba1d4f9cf47c74b2292586
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-12-14 14:16:32 +0200

    add image optimization

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
+    <section>
       <h2>Welcome to Home</h2>
       <p>This is the home page</p>
-    </div>
+      <Image src="/test1.jpg" alt="pingvin" width={300} height={300} />
+      {/* <Image src="/test.png" alt="smile" width={300} height={300} /> */}
+      {/* Зображення з зовнішніх джерел */}
+      <Image
+        src="https://picsum.photos/seed/picsum/300/300"
+        alt="test"
+        width={300}
+        height={300}
+      />
+    </section>
   );
 }
