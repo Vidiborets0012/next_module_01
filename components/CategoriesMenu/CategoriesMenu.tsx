e845dcd7d233000758a3cb35412e7738a8e767cb
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-12-22 08:13:46 +0200

    Add the CategoriesMenu component and implement it in Header

---
+++ components/CategoriesMenu/CategoriesMenu.tsx
@@ -0,0 +1,46 @@
+// components/CategoriesMenu/CategoriesMenu.tsx
+
+"use client";
+
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { Category, getCategories } from "@/lib/api";
+
+const CategoriesMenu = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false);
+
+  const toggle = () => setIsOpenMenu(!isOpenMenu);
+
+  // Додаємо стан
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  // Додаємо ефект для запиту
+  useEffect(() => {
+    // Змінюємо стан
+    getCategories().then((data) => setCategories(data));
+  }, []);
+
+  return (
+    <div>
+      <button onClick={toggle}>Notes</button>
+      {isOpenMenu && (
+        <ul>
+          <li>
+            <Link href={`/notes/filter/all`} onClick={toggle}>
+              All notes
+            </Link>
+          </li>
+          {categories.map((category) => (
+            <li key={category.id}>
+              <Link href={`/notes/filter/${category.id}`} onClick={toggle}>
+                {category.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default CategoriesMenu;
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,29 +1,37 @@
 import Link from "next/link";
-import css from "./Header.module.css";
 
-const Header = () => {
+import CategoriesMenu from "../CategoriesMenu/CategoriesMenu";
+
+const Header = async () => {
   return (
-    <header className={css.header}>
-      {/* <h2>NoteHub</h2> */}
+    <header>
+      <h2>NoteHub</h2>
       <Link href="/" aria-label="Home">
         NoteHub
       </Link>
       <nav aria-label="Main Navigation">
-        <ul className={css.navigation}>
-          {/* <li>Home</li> */}
-          <li>
+        <ul>
+          {/* <li>
             <Link href="/">Home</Link>
           </li>
           <li>
-            {/* <Link href="/notes">Notes</Link> */}
             <Link href="/notes/filter/all">Notes</Link>
-            {/* <Link href={`/notes/filter/${category.id}`}>Notes</Link> */}
+          </li> */}
+          <li>
+            <CategoriesMenu />
           </li>
           <li>
             <Link href="/profile">Profile</Link>
           </li>
           <li>
             <Link href="/about">About</Link>
+          </li>
+          {/* Нові посилання */}
+          <li>
+            <Link href="/sign-in">Login</Link>
+          </li>
+          <li>
+            <Link href="/sign-up">Register</Link>
           </li>
         </ul>
       </nav>
