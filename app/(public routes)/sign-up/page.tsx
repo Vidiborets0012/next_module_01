5ce949fa1e579c6334e51326df3423a83202ce47
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-12-22 09:12:12 +0200

    implement registration form submission (// app/(public routes)/sign-up/page.tsx)

--- app/(public routes)/sign-up/page.tsx
+++ app/(public routes)/sign-up/page.tsx
@@ -2,9 +2,35 @@
 
 "use client";
 
+// Додаємо імпорти
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { register, RegisterRequest } from "@/lib/api";
+import { ApiError } from "@/app/api/api";
+
 const SignUp = () => {
+  const router = useRouter();
+  const [error, setError] = useState("");
+
   const handleSubmit = async (formData: FormData) => {
-    // ...
+    try {
+      // Типізуємо дані форми
+      const formValues = Object.fromEntries(formData) as RegisterRequest;
+      // Виконуємо запит
+      const res = await register(formValues);
+      // Виконуємо редірект або відображаємо помилку
+      if (res) {
+        router.push("/profile");
+      } else {
+        setError("Invalid email or password");
+      }
+    } catch (error) {
+      setError(
+        (error as ApiError).response?.data?.error ??
+          (error as ApiError).message ??
+          "Oops... some error"
+      );
+    }
   };
 
   return (
@@ -25,6 +51,8 @@ const SignUp = () => {
         </label>
         <button type="submit">Register</button>
       </form>
+
+      {error && <p>{error}</p>}
     </>
   );
 };
